eaa887698cdc21f3669410b39d9f06ee7e86af34
oxcar103 <oxcar103@gmail.com>
2017-10-03 18:16:55 +0200

    Update script.sh Cifrado con Camellia y creación de la variable *vector*

--- Práctica 1/script.sh
+++ Práctica 1/script.sh
@@ -8,11 +8,12 @@ cat input.bin | head -c 2 > input1.bin && printf '\100' >> input1.bin && cat inp
 
 # Clave
 touch clave
+vector=67 # 103 = 0x67
 
 # Directorio que contendrá los archivos
 mkdir -p Resultados
 
-# Codificar con clave débil y semi-débil usando los modos ECB, CBC y OFB de DES.
+# Cifrar con clave débil y semi-débil usando los modos ECB, CBC y OFB de DES.
 for key in "0101010101010101" "011F011F010E010E"
     do
         for mode in "des-ecb" "des-cbc" "des-ofb"
@@ -24,15 +25,28 @@ for key in "0101010101010101" "011F011F010E010E"
                     sec="semiweak"
                 fi
 
-                openssl enc -$mode -K $key -iv 67 -in input.bin -out ./Resultados/input_$mode"_"$sec.bin
-                openssl enc -$mode -K $key -iv 67 -in input1.bin -out ./Resultados/input1_$mode"_"$sec.bin
+                openssl enc -$mode -K $key -iv $vector -in input.bin -out ./Resultados/input_$mode"_"$sec.bin
+                openssl enc -$mode -K $key -iv $vector -in input1.bin -out ./Resultados/input1_$mode"_"$sec.bin
             done
     done
 
-# Codificar con la clave generada usando los modos ECB y CBC de DES, AES-128 y AES-256.
+# Cifrar con la clave generada usando los modos ECB y CBC de DES, AES-128 y AES-256.
 for mode in "des-ecb" "des-cbc" "aes-128-ecb" "aes-128-cbc" "aes-256-ecb" "aes-256-cbc"
     do
-        openssl enc -$mode -k clave -iv 67 -in input.bin -out ./Resultados/input_$mode.bin
-        openssl enc -$mode -k clave -iv 67 -in input1.bin -out ./Resultados/input1_$mode.bin
+        openssl enc -$mode -k clave -iv $vector -in input.bin -out ./Resultados/input_$mode.bin
+        openssl enc -$mode -k clave -iv $vector -in input1.bin -out ./Resultados/input1_$mode.bin
+    done
+
+# Cifrar y descifrar con la clave generada usando AES-192 en modo OFB
+mode="aes-192-ofb"
+openssl enc -$mode -k clave -iv $vector -in input.bin -out ./Resultados/input_$mode.bin
+openssl enc -d -$mode -k clave -iv $vector -in ./Resultados/input_$mode.bin -out ./Resultados/output.bin
+openssl enc -$mode -k clave -iv $vector -in ./Resultados/output.bin -out ./Resultados/output_$mode.bin
+
+# Cifrar con la clave generada usando los modos ECB y CBC de Camellia-128, Camellia-192 y Camellia-256.
+for mode in "camellia-128-ecb" "camellia-128-cbc" "camellia-192-ecb" "camellia-192-cbc" "camellia-256-ecb" "camellia-256-cbc"
+    do
+        openssl enc -$mode -k clave -iv $vector -in input.bin -out ./Resultados/input_$mode.bin
+        openssl enc -$mode -k clave -iv $vector -in input1.bin -out ./Resultados/input1_$mode.bin
     done
 
