d19946802f0c87abb166c0e18b7b3c9697ce8ecc
oxcar103 <oxcar103@gmail.com>
2017-10-03 16:13:02 +0200

    Añadido a script.sh la creación de input.bin e input1.bin

--- Práctica 1/script.sh
+++ Práctica 1/script.sh
@@ -1,7 +1,18 @@
 #! /bin/bash
 
+# Archivo de 0's
+dd if=/dev/zero of=input.bin bs=128 count=1 
+
+# Archivo de 0's con un 1
+cat input.bin | head -c 2 > input1.bin && printf '\100' >> input1.bin && cat input.bin | tail -c 125 >> input1.bin
+
+# Clave
 touch clave
 
+# Directorio que contendrá los archivos
+mkdir Resultados
+
+# Codificar con clave débil y semi-débil usando los modos ECB, CBC y OFB de DES.
 for key in "0101010101010101" "011F011F010E010E"
     do
         for mode in "des-ecb" "des-cbc" "des-ofb"
@@ -18,9 +29,10 @@ for key in "0101010101010101" "011F011F010E010E"
             done
     done
 
-
+# Codificar con la clave generada usando los modos ECB y CBC de DES, AES-128 y AES-256.
 for mode in "des-ecb" "des-cbc" "aes-128-ecb" "aes-128-cbc" "aes-256-ecb" "aes-256-cbc"
     do
         openssl enc -$mode -k clave -iv 67 -in input.bin -out ./Resultados/input_$mode.bin
         openssl enc -$mode -k clave -iv 67 -in input1.bin -out ./Resultados/input1_$mode.bin
     done
+
