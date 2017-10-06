697b5f5d694a80ca4cb413bad224fabbe86c021f
oxcar103 <oxcar103@gmail.com>
2017-10-06 10:08:48 +0200

    Cambio de clave Generar a partir del archivo clave da problemas, fijada la clave 1032456789abcdef

--- Práctica 1/script.sh
+++ Práctica 1/script.sh
@@ -7,7 +7,6 @@ dd if=/dev/zero of=input.bin bs=128 count=1 2> /dev/null
 cat input.bin | head -c 2 > input1.bin && printf '\100' >> input1.bin && cat input.bin | tail -c 125 >> input1.bin
 
 # Clave
-touch clave
 vector=67 # 103 = 0x67
 
 # Directorio que contendrá los archivos
@@ -31,22 +30,24 @@ for key in "0101010101010101" "011F011F010E010E"
     done
 
 # Cifrar con la clave generada usando los modos ECB y CBC de DES, AES-128 y AES-256.
+key="1032456789abcdef"
+
 for mode in "des-ecb" "des-cbc" "aes-128-ecb" "aes-128-cbc" "aes-256-ecb" "aes-256-cbc"
     do
-        openssl enc -$mode -k clave -iv $vector -in input.bin -out ./Resultados/input_$mode.bin
-        openssl enc -$mode -k clave -iv $vector -in input1.bin -out ./Resultados/input1_$mode.bin
+        openssl enc -$mode -K $key -iv $vector -in input.bin -out ./Resultados/input_$mode.bin
+        openssl enc -$mode -K $key -iv $vector -in input1.bin -out ./Resultados/input1_$mode.bin
     done
 
 # Cifrar y descifrar con la clave generada usando AES-192 en modo OFB
 mode="aes-192-ofb"
-openssl enc -$mode -k clave -iv $vector -in input.bin -out ./Resultados/input_$mode.bin
-openssl enc -d -$mode -k clave -iv $vector -in ./Resultados/input_$mode.bin -out ./Resultados/output.bin
-openssl enc -$mode -k clave -iv $vector -in ./Resultados/output.bin -out ./Resultados/output_$mode.bin
+openssl enc -$mode -K $key -iv $vector -in input.bin -out ./Resultados/input_$mode.bin
+openssl enc -d -$mode -K $key -iv $vector -in ./Resultados/input_$mode.bin -out ./Resultados/output.bin
+openssl enc -$mode -K $key -iv $vector -in ./Resultados/output.bin -out ./Resultados/output_$mode.bin
 
 # Cifrar con la clave generada usando los modos ECB y CBC de Camellia-128, Camellia-192 y Camellia-256.
 for mode in "camellia-128-ecb" "camellia-128-cbc" "camellia-192-ecb" "camellia-192-cbc" "camellia-256-ecb" "camellia-256-cbc"
     do
-        openssl enc -$mode -k clave -iv $vector -in input.bin -out ./Resultados/input_$mode.bin
-        openssl enc -$mode -k clave -iv $vector -in input1.bin -out ./Resultados/input1_$mode.bin
+        openssl enc -$mode -K $key -iv $vector -in input.bin -out ./Resultados/input_$mode.bin
+        openssl enc -$mode -K $key -iv $vector -in input1.bin -out ./Resultados/input1_$mode.bin
     done
 
