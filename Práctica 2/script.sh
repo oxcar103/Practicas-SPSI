80998f2386b31b62999a42082e0a2ba81e9d9190
oxcar103 <oxcar103@gmail.com>
2017-10-27 11:01:50 +0200

    Metiendo los archivos en directorios

--- Práctica 2/script.sh
+++ Práctica 2/script.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
+# Directorios que contendrán los archivos
+mkdir -p Resultados
+mkdir -p Claves
+
 # Archivo de 0's
-dd if=/dev/zero of=input.bin bs=128 count=1 2> /dev/null
+dd if=/dev/zero of=./Resultados/input.bin bs=128 count=1 2> /dev/null
 
 key_size="768"
 name=${1:-My}       # Primer parámetro, por defecto "My"
@@ -11,27 +15,27 @@ fkey="sessionkey"
 mode="-aes-256-cbc"
 
 # Generación de clave RSA
-openssl genrsa -out $name"RSAkey.pem" $key_size
-openssl rsa -aes128 -passout pass:$passwd -in $name"RSAkey.pem" -out $name"RSApriv.pem"
-openssl rsa -pubout -in $name"RSAkey.pem" -out $name"RSApub.pem"
+openssl genrsa -out ./Claves/$name"RSAkey.pem" $key_size
+openssl rsa -aes128 -passout pass:$passwd -in ./Claves/$name"RSAkey.pem" -out ./Claves/$name"RSApriv.pem"
+openssl rsa -pubout -in ./Claves/$name"RSAkey.pem" -out ./Claves/$name"RSApub.pem"
 
 # Cifrar con clave pública
 # openssl rsautl -encrypt -passin pass:$passwd -pubin -inkey $name"RSApub.pem" -in "input.bin" -out "input_RSA.bin"
 
 # Cifrado híbrido
 # Cifrando...
-openssl rand 32 -hex -out $fkey         # 32 caracteres hexadecimales = 256 de tamaño de clave
-echo $mode >> $fkey
-openssl rsautl -encrypt -pubin -inkey $name"RSApub.pem" -in $fkey -out $fkey".enc"
-openssl enc $mode -pass file:$fkey -in "input.bin" -out "output.bin"
+openssl rand 32 -hex -out ./Resultados/$fkey         # 32 caracteres hexadecimales = 256 de tamaño de clave
+echo $mode >> ./Resultados/$fkey
+openssl rsautl -encrypt -pubin -inkey ./Claves/$name"RSApub.pem" -in ./Resultados/$fkey -out ./Resultados/$fkey".enc"
+openssl enc $mode -pass file:./Resultados/$fkey -in ./Resultados/"input.bin" -out ./Resultados/"output.bin"
 
 # Descifrando...
-openssl rsautl -decrypt -passin pass:$passwd -inkey $name"RSApriv.pem" -in $fkey".enc" -out $fkey".dec"
-dec_mode=`cat $fkey".dec" | tail -n 1`
-openssl enc -d $dec_mode -pass file:$fkey".dec" -in "output.bin" -out "output_dec.bin"
+openssl rsautl -decrypt -passin pass:$passwd -inkey ./Claves/$name"RSApriv.pem" -in ./Resultados/$fkey".enc" -out ./Resultados/$fkey".dec"
+dec_mode=`cat ./Resultados/$fkey".dec" | tail -n 1`
+openssl enc -d $dec_mode -pass file:./Resultados/$fkey".dec" -in ./Resultados/"output.bin" -out ./Resultados/"output_dec.bin"
 
 # Generación de clave por curva elíptica
-openssl ecparam -name $curve -out "stdECparam.pem"
-openssl ecparam -in "stdECparam.pem" -genkey -out $name"ECkey.pem" 
-openssl ec -des3 -passout pass:$passwd -in $name"ECkey.pem" -out $name"ECpriv.pem"
-openssl ec -pubout -in $name"ECkey.pem" -out $name"ECpub.pem"
+openssl ecparam -name $curve -out ./Claves/"stdECparam.pem"
+openssl ecparam -in ./Claves/"stdECparam.pem" -genkey -out ./Claves/$name"ECkey.pem"
+openssl ec -des3 -passout pass:$passwd -in ./Claves/$name"ECkey.pem" -out ./Claves/$name"ECpriv.pem"
+openssl ec -pubout -in ./Claves/$name"ECkey.pem" -out ./Claves/$name"ECpub.pem"
