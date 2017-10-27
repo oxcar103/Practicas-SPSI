6e28b401fd2881f9ba8de1d57e6cdcc7d1eb4c02
oxcar103 <oxcar103@gmail.com>
2017-10-27 09:26:06 +0200

    Encriptado con curva elíptica

--- Práctica 2/script.sh
+++ Práctica 2/script.sh
@@ -6,10 +6,16 @@ dd if=/dev/zero of=input.bin bs=128 count=1 2> /dev/null
 key_size="768"
 name=${1:-My}       # Primer parámetro, por defecto "My"
 passwd="0123456789"
-
+curve="prime192v1"
 
 
 # Generación de clave RSA
 openssl genrsa -out $name"RSAkey.pem" $key_size
 openssl rsa -aes128 -passout pass:$passwd -in $name"RSAkey.pem" -out $name"RSApriv.pem"
 openssl rsa -pubout -in $name"RSAkey.pem" -out $name"RSApub.pem"
+
+# Generación de clave por curva elíptica
+openssl ecparam -name $curve -out "stdECparam.pem"
+openssl ecparam -in "stdECparam.pem" -genkey -out $name"ECkey.pem" 
+openssl ec -des3 -passout pass:$passwd -in $name"ECkey.pem" -out $name"ECpriv.pem"
+openssl ec -pubout -in $name"ECkey.pem" -out $name"ECpub.pem"
