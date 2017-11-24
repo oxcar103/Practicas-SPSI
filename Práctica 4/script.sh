6645c165c19a254cc51ee998f6d10f60c3b2f86d
oxcar103 <oxcar103@gmail.com>
2017-11-24 21:40:49 +0100

    Script CA.pl

--- Práctica 4/script.sh
+++ Práctica 4/script.sh
@@ -3,7 +3,8 @@
 # Directorios que contendrán los archivos
 mkdir -p Resultados Claves/certs Claves/crl Claves/newcerts Claves/private
 
-conf="-config my_openssl.cnf"
+conf="-config my_openssl.cnf"       # Archivo de configuración personal
+CA_pl=/usr/lib/ssl/misc/CA.pl       # Dirección del script CA.pl
 
 # Archivos recomendados para el CA
 echo 1000 > ./Claves/serial
@@ -11,4 +12,4 @@ touch ./Claves/index.txt
 
 # Generamos la CA raíz, que es un certificado auto-firmado
 openssl req -new -x509 -days 103 -config my_openssl.cnf #-keyout private/cakey.pem -out cacert.pem
-
+# $CA_pl -newcert       # Equivalente usando el script CA.pl
