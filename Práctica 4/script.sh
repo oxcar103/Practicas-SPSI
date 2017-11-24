f0c8205aaf52071a7cfac734ce2320960ef70e00
oxcar103 <oxcar103@gmail.com>
2017-11-24 10:20:01 +0100

    Archivos de CA Aún no entiendo para qué sirven pero en todos lados, los ponen así

--- Práctica 4/script.sh
+++ Práctica 4/script.sh
@@ -5,3 +5,7 @@ mkdir -p Resultados Claves/certs Claves/crl Claves/newcerts Claves/private
 
 conf="-config my_openssl.cnf"
 
+# Archivos recomendados para el CA
+echo 1000 > ./Claves/serial
+touch ./Claves/index.txt
+
