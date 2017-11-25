80b2d527d1c21cc99a62b8422bfbd01ff60c1442
oxcar103 <oxcar103@gmail.com>
2017-11-25 18:14:22 +0100

    Quitando los parámetros del script

--- Práctica 4/script.sh
+++ Práctica 4/script.sh
@@ -4,8 +4,7 @@
 mkdir -p Resultados Claves/certs Claves/crl Claves/newcerts Claves/private
 
 conf="-config my_openssl.cnf"       # Archivo de configuración personal
-# Parámetros personales del certificado
-param="/C=ES/ST=Andalusia/L=Granada/O=Oxcar103 Inc./OU=Oxcar103 Inc. Certificate Authority/CN=oxcar103/emailAddress=oxcar103@gmail.com"
+param=$(cat param.txt)              # Parámetros personales del certificado
 CA_pl=/usr/lib/ssl/misc/CA.pl       # Dirección del script CA.pl
 passwd="0123456789"
 
@@ -14,6 +13,6 @@ echo 1000 > ./Claves/serial
 touch ./Claves/index.txt
 
 # Generamos la CA raíz, que es un certificado auto-firmado
-openssl req -new -passout pass:$passwd -x509 -days 103 $conf -subj "$param" #-keyout private/cakey.pem -out cacert.pem
+openssl req -new -passout pass:$passwd -x509 -days 103 $conf -subj "$param" -keyout Claves/private/cakey.pem -out Claves/cacert.pem
 # $CA_pl -newcert       # Equivalente usando el script CA.pl, salvo porque no le podemos pasar los parámetros por línea de comandos
 
