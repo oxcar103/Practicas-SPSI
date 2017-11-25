21ddf5badeaaa4793e3afbea7d9323cc4eeda21f
oxcar103 <oxcar103@gmail.com>
2017-11-25 20:21:59 +0100

    Clave DSA

--- Práctica 4/script.sh
+++ Práctica 4/script.sh
@@ -7,9 +7,10 @@ conf="-config my_openssl.cnf"       # Archivo de configuración personal
 param=$(cat param.txt)              # Parámetros personales del certificado
 CA_pl=/usr/lib/ssl/misc/CA.pl       # Dirección del script CA.pl
 passwd="0123456789"
+size=1024
 
-# Archivos recomendados para el CA
-echo 1000 > ./Claves/serial
+# Archivos necesarios para el CA
+echo "1000" > Claves/serial
 touch ./Claves/index.txt
 
 # Generamos la CA raíz, que es un certificado auto-firmado
@@ -17,7 +18,14 @@ openssl req -new -passout pass:$passwd -x509 -days 103 $conf -subj "$param" -key
 # $CA_pl -newcert       # Equivalente usando el script CA.pl, salvo porque no le podemos pasar los parámetros por línea de comandos
 
 # Generamos una nueva solicitud de certificado
-openssl req -new -passout pass:$passwd $conf -subj "$param" -keyout Claves/private/newkey.pem -out Claves/requests/default_key.pem
+openssl req -new -passout pass:$passwd $conf -subj "$param" -keyout Claves/private/default_key.pem -out Claves/requests/default_key.pem
 
 # Certificamos la nueva solicitud
 openssl ca -batch -passin pass:$passwd $conf -in Claves/requests/default_key.pem -out Claves/newcerts/default_key.pem
+
+# Generamos una clave DSA igual que en la práctica 3
+openssl dsaparam -out ./Claves/sharedDSA.pem $size
+openssl gendsa -out ./Claves/DSAkey.pem ./Claves/sharedDSA.pem
+openssl dsa -aes128 -passout pass:$passwd -in ./Claves/DSAkey.pem -out ./Claves/private/DSApriv.pem
+openssl dsa -pubout -in ./Claves/DSAkey.pem -out ./Claves/DSApub.pem
+
