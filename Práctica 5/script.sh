5efac006d383212a3a7d5b9d4bf03927e8e0f9d6
oxcar103 <oxcar103@gmail.com>
2017-12-17 17:26:01 +0100

    Comentando el código

--- Práctica 5/script.sh
+++ Práctica 5/script.sh
@@ -3,27 +3,27 @@
 # Directorios que contendrán los archivos
 mkdir -p Resultados Gráficas
 
-sample=${1:-10}
-text=${2:-"Texto de prueba"}
-last=${3:-3}
-first=${4:-1}
-increment=${5:-1}
+sample=${1:-10}                         # Primer parámetro: número de muestras, por defecto 10
+text=${2:-"Texto de prueba"}            # Segundo parámetro: texto de entrada, por defecto "Texto de prueba"
+last=${3:-3}                            # Tercer parámetro: último valor, por defecto 3
+first=${4:-1}                           # Cuarto parámetro: primer valor, por defecto 1
+increment=${5:-1}                       # Quinto parámetro: incremento entre valores, por defecto 1
 
 random="Resultados\random"
 linear="Resultados\linear"
 
+# Bucle de los valores
 for i in `seq $first $increment $last`
     do
+        # Inicializamos vacíos los archivos de resultados
         touch $random$i".csv" 2> $random$i".csv"
         touch $linear$i".csv" 2> $linear$i".csv"
 
+        # Bucle de las muestras
         for j in `seq $sample`
             do
-                echo "Prueba $j de $i ceros"
+                #echo "Prueba $j de $i ceros"        # Para visualizar el progreso
                 ./functions.sh $text $i $random$i".csv" $linear$i".csv"
             done
     done
-
-
-
 
