e25ed0f7b1f62868c979dec535d58d6ca3875440
oxcar103 <oxcar103@gmail.com>
2018-01-24 12:43:43 +0100

    Corrección del script En un ejercicio nos piden salida binaria

--- Práctica 3/script.sh
+++ Práctica 3/script.sh
@@ -31,7 +31,7 @@ for i in $name $surname
 
 # Generación de valores Hash
 openssl dgst -sha384 -c -out ./Resultados/$name"DSApub.sha384" ./Claves/$name"DSApub.pem"
-openssl dgst -ripemd160 -c -out ./Resultados/$surname"DSApub.ripemd160" ./Claves/$surname"DSApub.pem"
+openssl dgst -ripemd160 -bin -out ./Resultados/$surname"DSApub.ripemd160" ./Claves/$surname"DSApub.pem"
 openssl dgst -hmac "12345" -c -out ./Resultados/"sharedDSA.hmac" ./Claves/"sharedDSA.pem"
 
 # Simulación del protocolo Estación a Estación
